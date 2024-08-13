df19086ad5ec3ab4dcf1c49be21e12be1f9483ee
George <yhayoo@gmail.com>
2024-08-13 13:47:40 -0400

    Create ex_icon.py

---
+++ ex_icon.py
@@ -0,0 +1,14 @@
+# Flet Icon
+# 2024-08-13
+#
+import flet as ft
+
+def main(page: ft.Page):
+    page.add(
+        ft.Icon(name=ft.icons.FAVORITE, color="red", size=50),
+        ft.Icon(name=ft.icons.FAVORITE, color="red", size=50, opacity=0.2),
+        ft.Icon(name=ft.icons.FAVORITE, color="red", size=50, semantics_label="Heart"),
+        ft.Icon(name=ft.icons.FAVORITE, color="red", size=50, tooltip="Heart"),
+        ft.Icon(name=ft.icons.FAVORITE, color="red", size=50, rotate=45),
+    )
+ft.app(target=main)
