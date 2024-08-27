581f566e0d7ff494513bfa775e78e35dbf3aed66
George <yhayoo@gmail.com>
2024-08-27 10:16:29 -0400

    Update ex_chicken_rabbit.py

--- ex_chicken_rabbit.py
+++ ex_chicken_rabbit.py
@@ -9,16 +9,12 @@ def main(page: ft.Page):
     def calculate(e):
         try:
             H = int(H_input.value)
-            F = int(F_input.value)
-           
+            F = int(F_input.value)           
             C=  int((4 * H - F )/2)
-            R = H - C
-           
-
+            R = H - C  
             result.value = f"雞有 {C} 隻，兔子有 {R} 隻"
         except ValueError:
             result.value = "請輸入數字"
-
         page.update()
 
     H_input = ft.TextField(label="雞兔同籠共幾隻", icon=ft.icons.CRUELTY_FREE)
@@ -26,13 +22,11 @@ def main(page: ft.Page):
     calculate_button = ft.ElevatedButton(text="計算", on_click=calculate)
     result = ft.Text()
 
-    page.add(
-        
+    page.add(        
             H_input,
             F_input,
             calculate_button,
-            result
-        
+            result        
     )
 
 ft.app(target=main)
